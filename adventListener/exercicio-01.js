1a9ff60d23fdd4732cabe9a056f1c2638ccabe7e
edilsonjotasilva <edilsonjotasilva@gmail.com>
2026-06-23 21:33:43 -0300

    atualizado

--- adventListener/exercicio-01.js
+++ adventListener/exercicio-01.js
@@ -1,13 +1,28 @@
 const caixa1 = document.querySelector('#caixa1');
 const caixa2 = document.querySelector('#caixa2');
-const btn = document.querySelector('#btn_copiar');
+const btn_copiar = document.querySelector('#btn_copiar');
+const btn_retornar = document.querySelector('#btn_retornar');
 const todosCursos = [...document.querySelectorAll('.curso')];
 
-todosCursos.forEach(curso => {
-    curso.addEventListener('click', (evt) => {
-        curso = evt.target;
+todosCursos.map((el) => {
+    el.addEventListener('click', (evt) => {
+        const curso = evt.target;
         curso.classList.toggle('selecionado');
-        console.log(curso);
     });
 });
+
+btn_copiar.addEventListener('click', () => {
+    const cursosSelecionados = [...document.querySelectorAll('.selecionado')];
+    cursosSelecionados.map((el) => {    
+        caixa2.appendChild(el);
+    });
+});
+btn_retornar.addEventListener('click', () => {
+    const cursosSelecionadosRetorno = [...document.querySelectorAll('.selecionado')];
+    cursosSelecionadosRetorno.map((el) => {    
+        caixa1.appendChild(el);
+    });
+});
+
+
 
