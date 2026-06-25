59fa67993a3df41b43b9998f5b96c28d75d34cba
edilsonjotasilva <edilsonjotasilva@gmail.com>
2026-06-25 15:33:38 -0300

    adcionando no DOM

--- DOM/exemplo-dom.js
+++ DOM/exemplo-dom.js
@@ -1,6 +1,7 @@
 const todosCursos = [...document.getElementsByClassName("curso")];
 const cursosC1 = [...document.getElementsByClassName("c1")];
 const cursosC2 = [...document.getElementsByClassName("c2")];
+const caixa1 = document.getElementById("caixa1");
 
 // todosCursos.map(curso => {
     //     curso.addEventListener("click", (evento) => {    
@@ -19,8 +20,23 @@ const cursosC2 = [...document.getElementsByClassName("c2")];
 // });
 ////////////////////////////////////////////
 
-cursosC1.map((curso, index) => {
-    if(curso.innerHTML === "React"){
-        console.log("curso " +cursosC1[index].innerHTML+ " encontrado na posição " + index);
+// cursosC1.map((curso, index) => {
+//     if(curso.innerHTML === "React"){
+//         console.log("curso " +cursosC1[index].innerHTML+ " encontrado na posição " + index);
+//     }
+// });
+const cursos = ["HTML", "CSS", "JavaScript", "React", "Node.js", "Python", "Mysql", "Postgresql", "Rubi", "Arduino", "C++", "PHP"];
+cursos.map((ele, index) => {
+    const novoElemento = document.createElement("div");
+    novoElemento.classList.add("curso", "c1");
+    if(index > 5){
+        novoElemento.classList.add("curso", "c2");
     }
+    if(index == 8){
+        novoElemento.addEventListener("click", (evento) => {
+            novoElemento.classList.toggle("selecionado");
+        });
+    }
+    novoElemento.innerHTML = ele;
+    caixa1.appendChild(novoElemento);
 });
